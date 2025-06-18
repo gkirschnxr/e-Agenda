9011b024c53b40fd6793d20a9aaee386c4381cdf
gkirschnxr <kirschnxr@gmail.com>
2025-06-18 14:21:30 -0300

    Finalizado Menu Inicial (index) e Cadastrar

--- e-Agenda.WebApp/Controllers/ContatoController.cs
+++ e-Agenda.WebApp/Controllers/ContatoController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using e_Agenda.Dominio.ModuloContato;
+using e_Agenda.Infraestrutura.Arquivos.Compartilhado;
+using e_Agenda.Infraestrutura.Arquivos.ModuloContato;
+using e_Agenda.WebApp.Extensions;
+using e_Agenda.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace e_Agenda.WebApp.Controllers;
 
 [Route("contatos")]
 public class ContatoController : Controller
 {
+    private readonly ContextoDados contexto;
+    private readonly IRepositorioContato repositorioContato;
+
+    public ContatoController() {
+        contexto = new ContextoDados(true);
+        repositorioContato = new RepositorioContato(contexto);
+    }
+
     [HttpGet]
-    public IActionResult Index() {
-        return View();
+    public IActionResult Index() { 
+        var registros = repositorioContato.SelecionarRegistros();
+
+        var visualizarVM = new VisualizarContatosViewModel(registros);
+
+        return View(visualizarVM); 
+    }
+
+    [HttpGet("cadastrar")]
+    public IActionResult Cadastrar() {
+        var cadastrarVM = new CadastrarContatoViewModel();
+
+        return View(cadastrarVM);
+    }
+
+    [HttpPost("cadastrar")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Cadastrar(CadastrarContatoViewModel cadastrarVM) {
+        var registro = cadastrarVM.ParaEntidade();
+
+        repositorioContato.CadastrarRegistro(registro);
+    
+        return RedirectToAction(nameof(Index));
     }
 }
---
+++ e-Agenda.WebApp/Models/ContatoViewModels.cs
@@ -0,0 +1,53 @@
+﻿using e_Agenda.Dominio.ModuloContato;
+using e_Agenda.WebApp.Extensions;
+
+namespace e_Agenda.WebApp.Models;
+
+public abstract class FormularioContatoViewModel {
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+
+    //public string Cargo { get; set; }
+    //public string Empresa { get; set; }
+}
+
+public class CadastrarContatoViewModel : FormularioContatoViewModel {
+    public CadastrarContatoViewModel() { }
+    public CadastrarContatoViewModel(string nome, string email, string telefone) : this() {
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+        //Cargo = cargo;
+        //Empresa = empresa;
+    }
+}
+
+public class VisualizarContatosViewModel {
+    public List<DetalhesContatoViewModel> Registros { get; set; }
+
+    public VisualizarContatosViewModel(List<Contato> contatos) {
+        Registros = [];
+
+        foreach (var c in contatos) {
+            var detalhesVM = c.ParaDetalhesVM();
+
+            if (detalhesVM != null)
+               Registros.Add(detalhesVM);
+        }
+    }
+}
+
+public class DetalhesContatoViewModel {
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+
+    public DetalhesContatoViewModel(Guid id, string nome, string email, string telefone) {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+    }
+}
