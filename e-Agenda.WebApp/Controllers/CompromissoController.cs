803b567408bdf6da79c46bea4369a1e7fb7f78b9
gkirschnxr <kirschnxr@gmail.com>
2025-06-20 16:39:23 -0300

    Atribuido Menu Inicial (index) para Compromissos

---
+++ e-Agenda.WebApp/Controllers/CompromissoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_Agenda.WebApp.Controllers;
+
+[Route("compromissos")]
+public class CompromissoController : Controller
+{
+    [HttpGet]
+    public IActionResult Index() {
+        return View();
+    }
+}
