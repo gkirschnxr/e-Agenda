3338cae43f578f9cceabb0fa430326ca057a6bff
gkirschnxr <kirschnxr@gmail.com>
2025-06-20 18:10:21 -0300

    Atribuido Detalhes para Compromissos

--- e-Agenda.WebApp/Controllers/CompromissoController.cs
+++ e-Agenda.WebApp/Controllers/CompromissoController.cs
@@ -1,6 +1,7 @@
 ﻿using e_Agenda.Dominio.ModuloCompromissos;
 using e_Agenda.Infraestrutura.Arquivos.Compartilhado;
 using e_Agenda.Infraestrutura.Arquivos.ModuloCompromisso;
+using e_Agenda.Infraestrutura.Arquivos.ModuloContato;
 using e_Agenda.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,5 +26,15 @@ public class CompromissoController : Controller
         var visualizarVM = new VisualizarCompromissosViewModel(registros);
 
         return View(visualizarVM);
+    }
+
+    [HttpGet("detalhes/{id:guid}")]
+    public IActionResult Detalhes(Guid id) {
+        var registro = repositorioCompromisso.SelecionarRegistroPorId(id);
+
+        var detalhesVM = new DetalhesCompromissoViewModel(id, registro.Assunto, registro.DataOcorrencia, registro.HoraInicio,
+                                                         registro.HoraTermino, registro.Tipo, registro.Contato);
+
+        return View(detalhesVM);
     }
 }
