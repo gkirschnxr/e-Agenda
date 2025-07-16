8af59edbb3fcae83853a279ce2047b2b84e7682c
gkirschnxr <kirschnxr@gmail.com>
2025-07-16 11:35:36 -0300

    Implementada config de TbMesa e DbContext

---
+++ eAgenda.Infraestrutura.ORM/ModuloContato/MapeadorContato.cs
@@ -0,0 +1,29 @@
+﻿using e_Agenda.Dominio.ModuloContato;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAgenda.Infraestrutura.ORM.ModuloContato;
+public class MapeadorContato : IEntityTypeConfiguration<Contato>
+{
+    public void Configure(EntityTypeBuilder<Contato> builder) {
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.Nome)
+            .IsRequired();
+
+        builder.Property(x => x.Email)
+            .IsRequired();
+
+        builder.Property(x => x.Telefone)
+            .IsRequired();
+
+        builder.Property(x => x.Empresa)
+            .IsRequired();
+
+        builder.Property(x => x.Cargo)
+            .IsRequired();
+    }
+}
